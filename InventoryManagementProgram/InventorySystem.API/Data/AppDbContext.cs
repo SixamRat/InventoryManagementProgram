25274bcab6ba92fa59a013eb4b49c4a09fc98137
Saga Martineau <saga.martineau@gmail.com>
2026-03-12 15:07:17 +0100

    Adding setup in classes and folders

---
+++ InventoryManagementProgram/InventorySystem.API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InventorySystem.API.Models;
+
+namespace InventorySystem.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Scale> Scales { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<WeightReading> WeightReadings { get; set; }
+        public DbSet<ChangeRequest> ChangeRequests { get; set; }
+    }
+}
