1834a7de22f77879fd7a44ab1e70a17ced4da09a
Saga Martineau <saga.martineau@gmail.com>
2026-03-21 11:22:05 +0100

    Connection http URL

--- InventoryManagementProgram/InventorySystem.Client/Program.cs
+++ InventoryManagementProgram/InventorySystem.Client/Program.cs
@@ -1,11 +1,26 @@
 using InventorySystem.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//MSAL-autentisering mot Entra ID
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(
+        builder.Configuration["ApiSettings:Scope"]!
+    );
+});
+
+builder.Services.AddHttpClient("InventoryAPI",
+    client => client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"]!))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("InventoryAPI"));
 
 await builder.Build().RunAsync();
