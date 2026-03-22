99b3819449db91690db24d9c6135839db056ed34
Saga Martineau <saga.martineau@gmail.com>
2026-03-22 16:17:45 +0100

    fixed JSON serializing

--- InventoryManagementProgram/InventorySystem.API/Program.cs
+++ InventoryManagementProgram/InventorySystem.API/Program.cs
@@ -27,7 +27,11 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod());
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
