dd5ffb335cf902561ec02d2db49c9ee5065470c4
Saga Martineau <saga.martineau@gmail.com>
2026-03-28 12:37:29 +0100

    Updating with swagger documentation

--- InventoryManagementProgram/InventorySystem.API/Program.cs
+++ InventoryManagementProgram/InventorySystem.API/Program.cs
@@ -33,7 +33,20 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "InventorySystem API",
+        Version = "v1",
+        Description = "REST API för lagerhållningssystem med nätverksanslutna vågar"
+    });
+
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+        options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
