76cc444c2a82e2401c1a284740781b07c1c70f99
Saga Martineau <saga.martineau@gmail.com>
2026-04-09 14:39:17 +0200

    creating JS scanner

---
+++ InventoryManagementProgram/InventorySystem.Client/wwwroot/qrScanner.js
@@ -0,0 +1,31 @@
+﻿let html5QtCode = null;
+
+window.QrScanner = {
+    start: function (dotNetHelper) {
+        html5QrCode = new Html5Qrcode("qr-reader");
+
+        html5QrCode.start(
+            { facingMode: "enviroment" },
+            {
+                fps: 10,
+                qrbox: { width: 250, height: 250 }
+            },
+            (decodedText) => {
+                // Skickar tillbaka QR-kod till Blazor
+                dotNetHelper.invokeMethodAsync("OnQrCodeScanned", decodedText);
+                // Stänger ner kameran efter scanning
+                html5QrCode.stop();
+            },
+            (errorMessage) => {
+                //Ignorera felaktig scanning
+            }
+        ).catch((err) => {
+            console.error("Kunde inte starta kameran:", err);
+        });
+    },
+    stop: function () {
+        if (html5QrCode) {
+            html5QrCode.stop().catch(err => console.log("Stop error:", err));
+        }
+    }
+};
