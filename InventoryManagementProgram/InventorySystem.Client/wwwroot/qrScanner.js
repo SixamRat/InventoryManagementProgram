118c750364b5b10d56fb2e25479faa8054f4dcff
Saga Martineau <saga.martineau@gmail.com>
2026-04-13 11:43:46 +0200

    Adding button to "start camera" Bypassing Blazor

--- InventoryManagementProgram/InventorySystem.Client/wwwroot/qrScanner.js
+++ InventoryManagementProgram/InventorySystem.Client/wwwroot/qrScanner.js
@@ -1,28 +1,34 @@
-﻿let html5QtCode = null;
+﻿let html5QrCode = null;
 
 window.QrScanner = {
     start: function (dotNetHelper) {
+        const readerDiv = document.getElementById("qr-reader");
+        if (!readerDiv) {
+            console.error("qr-reader div hittades inte");
+            return;
+        }
+
         html5QrCode = new Html5Qrcode("qr-reader");
 
-        html5QrCode.start(
-            { facingMode: "enviroment" },
-            {
-                fps: 10,
-                qrbox: { width: 250, height: 250 }
-            },
-            (decodedText) => {
-                // Skickar tillbaka QR-kod till Blazor
-                dotNetHelper.invokeMethodAsync("OnQrCodeScanned", decodedText);
-                // Stänger ner kameran efter scanning
-                html5QrCode.stop();
-            },
-            (errorMessage) => {
-                //Ignorera felaktig scanning
+        Html5Qrcode.getCameras().then(devices => {
+            if (devices && devices.length > 0) {
+                html5QrCode.start(
+                    { facingMode: "environment" },
+                    { fps: 10, qrbox: { width: 250, height: 250 } },
+                    (decodedText) => {
+                        dotNetHelper.invokeMethodAsync("OnQrCodeScanned", decodedText);
+                        html5QrCode.stop();
+                    },
+                    (errorMessage) => { }
+                );
+            } else {
+                console.error("Inga kameror hittades");
             }
-        ).catch((err) => {
-            console.error("Kunde inte starta kameran:", err);
+        }).catch(err => {
+            console.error("Kamerafel:", err);
         });
     },
+
     stop: function () {
         if (html5QrCode) {
             html5QrCode.stop().catch(err => console.log("Stop error:", err));
