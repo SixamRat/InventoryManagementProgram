304b48326f671e75c152c803125fd81e6bad5e0d
Saga Martineau <saga.martineau@gmail.com>
2026-05-05 15:41:34 +0200

    Adding helper to calculate weight to correct unit

---
+++ InventoryManagementProgram/InventorySystem.API/Helpers/ConversionHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace InventorySystem.API.Helpers
+{    
+    // Hjälpklass för att konvertera vikter till rätt enhet.
+    public static class ConversionHelper
+    {       
+        // Räknar om vikt i kg till angiven enhet med ConversionFactor.
+        public static double Convert(double weightInKg, double conversionFactor)
+        {
+            if (weightInKg < 0)
+                return 0;
+
+            return Math.Round(weightInKg * conversionFactor, 2);
+        }        
+        // Summerar flera vikter och avrundar till 2 decimaler.
+      
+        public static double SumConverted(IEnumerable<double> convertedValues)
+        {
+            return Math.Round(convertedValues.Sum(), 2);
+        }
+    }
+}
